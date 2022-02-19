f09203d7865c3f714506c834e576dad9676e46c5
Leonel <leonel.e.alderete@gmail.com>
2022-02-18 23:37:02 -0300

    se creo espress y puerto

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('prueba del backend');
+import express from 'express';
+
+//espress
+const app = express();
+//puerto
+app.set('port', process.env.PORT || 4000 );
+
+app.listen(app.get('port'), ()=>{
+    console.log('Estoy en el puerto'+ app.get('port'));
+})
