1c89fd3fe751188e4c06498f417195ea6a9372b7
alanChibilisco <alan.chibilisco@gmail.com>
2022-02-26 11:47:01 -0300

    se completo el get y post completos de turnos

--- src/controllers/turnos.controllers.js
+++ src/controllers/turnos.controllers.js
@@ -4,13 +4,41 @@ import { validateTextoEsp } from "../helpers/Validaciones";
 const turnoCtrl={};
 
 turnoCtrl.getTurno=async(req, res)=>{
-    res.send('aqui enviaria la lista de turnos');
+    //res.send('aqui enviaria la lista de turnos');
+    try {
+        const listTurnos=await Turno.find();
+        res.status(200).json(listTurnos);
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({
+            mensaje: "Error al intentar listar los turnos"
+        })
+    }
     
 };
 
 turnoCtrl.postTurno=async(req, res)=>{
-    res.send('estoy creando un turno');
-    console.log(req.body);
+    //res.send('estoy creando un turno');
+    //console.log(req.body);
+    try {
+        if (validateTextoEsp(req.body.detalleCita)&&validateTextoEsp(req.body.veterinario)&&validateTextoEsp(req.body.mascota)&&validateTextoEsp(req.body.startDate)) {
+            const newTurno= new Turno({
+                detalleCita: req.body.detalleCita,
+                veterinario: req.body.veterinario,
+                mascota: req.body.mascota,
+                startDate: req.body.startDate
+            });
+            await newTurno.save();
+            res.status(201).json({
+                mensaje: "El turno se creo correctamente"
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({
+            mensaje: "Error al intentar agregar un turno"
+        });
+    }
 };
 
 
