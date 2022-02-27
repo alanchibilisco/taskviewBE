b275a7cbb41dedb16157bf5d50370bfe77b43af6
alanChibilisco <alan.chibilisco@gmail.com>
2022-02-27 09:03:33 -0300

    se completo el get, put y delete de pacientes

--- src/controllers/pacientes.controllers.js
+++ src/controllers/pacientes.controllers.js
@@ -1,5 +1,9 @@
 import Paciente from "../models/pacientes";
-import { validateEmail, validateTexto, validateTelefono } from "../helpers/Validaciones";
+import {
+  validateEmail,
+  validateTexto,
+  validateTelefono,
+} from "../helpers/Validaciones";
 
 const pacienteCtrl = {};
 
@@ -19,24 +23,32 @@ pacienteCtrl.postPaciente = async (req, res) => {
   // console.log(req.body);
   try {
     //validar el (req.body) copiadas del front
-    if (validateTexto(req.body.nombreDueño)&&validateTexto(req.body.apellidoDueño)&&validateEmail(req.body.email)&&validateTelefono(req.body.telefono)&&validateTexto(req.body.nombreMascota)&&validateTexto(req.body.especieMascota)&&validateTexto(req.body.razaMascota)){
+    if (
+      validateTexto(req.body.nombreDueño) &&
+      validateTexto(req.body.apellidoDueño) &&
+      validateEmail(req.body.email) &&
+      validateTelefono(req.body.telefono) &&
+      validateTexto(req.body.nombreMascota) &&
+      validateTexto(req.body.especieMascota) &&
+      validateTexto(req.body.razaMascota)
+    ) {
       //crear un obj para guardar en la BBDD
-    const newPaciente = new Paciente({
-      nombreDueño: req.body.nombreDueño,
-      apellidoDueño: req.body.apellidoDueño,
-      email: req.body.email,
-      telefono: req.body.telefono,
-      nombreMascota: req.body.nombreMascota,
-      especieMascota: req.body.especieMascota,
-      razaMascota: req.body.razaMascota,
-    });
-    //una vez creado se guarda en la BBDD
-    await newPaciente.save();
-    //enviar resp al front end
-    res.status(201).json({
-      mensaje: "El producto se creo correctamente",
-    });
-    }      
+      const newPaciente = new Paciente({
+        nombreDueño: req.body.nombreDueño,
+        apellidoDueño: req.body.apellidoDueño,
+        email: req.body.email,
+        telefono: req.body.telefono,
+        nombreMascota: req.body.nombreMascota,
+        especieMascota: req.body.especieMascota,
+        razaMascota: req.body.razaMascota,
+      });
+      //una vez creado se guarda en la BBDD
+      await newPaciente.save();
+      //enviar resp al front end
+      res.status(201).json({
+        mensaje: "El producto se creo correctamente",
+      });
+    }
   } catch (error) {
     console.log(error);
     //envio de respuesta al front
@@ -45,5 +57,55 @@ pacienteCtrl.postPaciente = async (req, res) => {
     });
   }
 };
+
+pacienteCtrl.getPaciente = async (req, res) => {
+  try {
+    const pacienteBuscado = await Paciente.findById(req.params.id);
+    res.status(200).json(pacienteBuscado);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "No se pudo obtener el paciente buscado",
+    });
+  }
+};
+
+pacienteCtrl.putPaciente = async (req, res) => {
+  try {
+    if (
+      validateTexto(req.body.nombreDueño) &&
+      validateTexto(req.body.apellidoDueño) &&
+      validateEmail(req.body.email) &&
+      validateTelefono(req.body.telefono) &&
+      validateTexto(req.body.nombreMascota) &&
+      validateTexto(req.body.especieMascota) &&
+      validateTexto(req.body.razaMascota)
+    ) {      
+      await Paciente.findByIdAndUpdate(req.params.id, req.body);
+      res.status(200).json({
+        mensaje: "El paciente se actualizo correctamente",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "Error al actualizar el paciente",
+    });
+  }
+};
+
+pacienteCtrl.deletePaciente=async(req, res)=>{
+  try {
+    await Paciente.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      mensaje: "Se elimino el paciente deseado"
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(200).json({
+      mensaje: "Se elimino el paciente deseado"
+    });
+  }
+}
 
 export default pacienteCtrl;
--- src/routes/pacientes.routes.js
+++ src/routes/pacientes.routes.js
@@ -8,4 +8,10 @@ router
   .get(pacienteCtrl.getPacientes)
   .post(pacienteCtrl.postPaciente);
 
+router
+  .route("/pacientes/:id")
+  .get(pacienteCtrl.getPaciente)
+  .put(pacienteCtrl.putPaciente)
+  .delete(pacienteCtrl.deletePaciente);
+
 export default router;
