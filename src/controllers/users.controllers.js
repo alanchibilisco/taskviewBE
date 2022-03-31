4b227fe5bb80ba4e6aa96f44969182b05c91a6de
alanChibilisco <alan.chibilisco@gmail.com>
2022-03-31 20:05:03 -0300

    se completaron get, put, delete de users

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -36,4 +36,46 @@ usersCtrl.postUser=async(req, res)=>{
     }
 };
 
+usersCtrl.getUser=async(req, res)=>{
+    try {
+        const findUser=await Users.findById(req.params.id);
+        res.status(200).json(findUser);
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({
+            mensaje: "Error al obtener el usuario"
+        })
+    }
+};
+
+usersCtrl.putUser=async(req, res)=>{
+    try {
+        if (testEmail(req.body.userName)&&testPass(req.body.password)) {
+            await Users.findByIdAndUpdate(req.params.id, req.body);
+            res.status(200).json({
+                mensaje: "Se actualizaron los datos del usuario"
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({
+            mensaje: "No se pudo actualizar los datos del usuario"
+        })
+    }
+};
+
+usersCtrl.deleteUser=async(req,res)=>{
+    try {
+        await Users.findByIdAndDelete(req.params.id);
+        res.status(200).json({
+            mensaje: "Se elimino el usuario"
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({
+            mensaje: "No se pudo eliminar el usuario"
+        })
+    }
+}
+
 export default usersCtrl;
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,9 +1,13 @@
-import {Router} from "express";
+import { Router } from "express";
 import usersCtrl from "../controllers/users.controllers";
 
-const routeUsers=Router();
+const routeUsers = Router();
 
 routeUsers.route("/users").get(usersCtrl.getUsers).post(usersCtrl.postUser);
 
-
+routeUsers
+  .route("/users/:id")
+  .get(usersCtrl.getUser)
+  .put(usersCtrl.putUser)
+  .delete(usersCtrl.deleteUser);
 export default routeUsers;
