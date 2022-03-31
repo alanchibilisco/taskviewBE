7b570d8d440b227ff64ede279de907f922e23306
alanChibilisco <alan.chibilisco@gmail.com>
2022-03-31 20:09:48 -0300

    se purgo los componentes

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import path from 'path';
-// import router from './routes/pacientes.routes';
-// import routeTurno from './routes/turnos.routes';
-// import routeUser from './routes/users.routes';
-// import routeComentario from './routes/comentarios.routes';
 import routeSuperUser from './routes/superUser.routes';
 import routeUsers from './routes/users.routes';
 import './database'
@@ -28,9 +24,5 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'../public')))
 
 //ruta prueba
-// app.use('/apirollingvet', router);
-// app.use('/apirollingvet', routeTurno);
-// app.use('/apirollingvet', routeUser);
-// app.use('/apirollingvet', routeComentario);
 app.use('/movieapp', routeSuperUser);
 app.use('/movieapp', routeUsers);
